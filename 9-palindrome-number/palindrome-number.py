337395fcc098e24cad073bc64673f67daed6582e
Muhaiminul coding club <148750127+Muhaiminul-MCC@users.noreply.github.com>
2025-02-02 12:40:09 +0600

    Time: 0 ms (100.00%) | Memory: 17.9 MB (15.96%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         reversed_num = str(x)[::-1]  # Reverse the number as a string
-        return  str(x) == reversed_num
+        return  str(x) == reversed_num  # Compare the string versions
 
 x = 121
 solution = Solution()
