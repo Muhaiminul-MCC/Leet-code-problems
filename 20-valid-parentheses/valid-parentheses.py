1b7886ebd13de409e76c57588c29edfd782cfdc8
Muhaiminul coding club <148750127+Muhaiminul-MCC@users.noreply.github.com>
2025-02-06 13:15:35 +0600

    Time: 35 ms (5.08%) | Memory: 12.4 MB (88.33%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        while '()' in s or '{}' in s or '[]' in s:
+            s = s.replace('()', '').replace('{}', '').replace('[]', '')
+        
+        return s == ""
+
+s = "()"
+solution = Solution()
+print(solution.isValid(s))
